bc8e56e941ee864e4aecc2e48bd2fa86691af6fd
diptigandhi24 <gandhidipti.s@gmail.com>
2019-05-05 17:59:22 +0530

    Basic Implemantation of hooks and server call to load the list of player

---
+++ src/Components/PlayerList.js
@@ -0,0 +1,39 @@
+//Dipslay player list from ther server
+import React, { useState, useEffect } from "react";
+import { superHerosData } from "../data/ApiConfig";
+//connect to server and get the data from the server
+import axios from "axios";
+import { async } from "q";
+import "../App.css";
+
+export default function DisplayPlayersList() {
+  let [listOfSuperheros, displayCurrentList] = useState([]);
+  useEffect(async () => {
+    const result = await axios.get(superHerosData);
+    displayCurrentList(result.data.superheroes);
+  }, []);
+
+  const deleteItemFromList = event => {
+    const updateList = listOfSuperheros.filter(
+      elm => elm.image.url !== event.target.src
+    );
+    displayCurrentList(updateList);
+  };
+
+  return (
+    <div className="cards-display">
+      {listOfSuperheros.map(elem => {
+        return (
+          <div>
+            <img
+              src={elem.image.url}
+              onClick={deleteItemFromList}
+              className="cards"
+            />
+            <h2>{elem.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
